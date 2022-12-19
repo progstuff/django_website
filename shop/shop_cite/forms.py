d608e1f92d750717c9130b558871f583c52fa14b
Andrey Morozov <yerdnavozorom@gmail.com>
2022-12-19 23:08:27 +0300

    add registration post request

--- shop/shop_cite/forms.py
+++ shop/shop_cite/forms.py
@@ -1,10 +1,30 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class RegistrationForm(forms.Form):
-    name = forms.CharField(label=_('ФИО'))
-    phone = forms.CharField(label=_('Телефон'))
     email = forms.CharField(label=_('E-mail'), widget=forms.EmailInput)
     password = forms.CharField(label=_('Пароль'), widget=forms.PasswordInput)
     password_confirm = forms.CharField(label=_('Подтверждение пароля'), widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        p1 = self.cleaned_data.get('password', '')
+        p2 = self.cleaned_data.get('password_confirm', '')
+        if p1 != p2 and p1 != '' and p2 != '':
+            raise ValidationError(_("Пароли не совпадают"))
+        if p2 == '':
+            raise ValidationError(_("Пароль не введен"))
+        return p2
+
+    def get_error_messages(self):
+        errors = {'email': '',
+                  'password': '',
+                  'password_confirm': ''}
+        try:
+            self.clean_password_confirm()
+        except ValidationError:
+            if self.cleaned_data.get('password', '') != '':
+                errors['password_confirm'] = 'Пароли не совпадают'
+
+        return errors
--- shop/shop_cite/views/registration_page.py
+++ shop/shop_cite/views/registration_page.py
@@ -1,13 +1,25 @@
 from .view_utils import BaseTemplate
 from ..forms import RegistrationForm
+from django.core.exceptions import ValidationError
 
 
 class RegistrationPage(BaseTemplate):
 
     def get(self, request):
         form = RegistrationForm()
-        a = form.visible_fields()
-        b = 1
         return self.get_render(request,
                                'shop_cite/registration.html',
                                context={'form': form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            return self.get_render(request,
+                                   'shop_cite/index.html',
+                                   context={'form': form, 'errors': ''})
+        else:
+            errors = form.get_error_messages()
+
+            return self.get_render(request,
+                                   'shop_cite/registration.html',
+                                   context={'form': form, 'errors': errors})
