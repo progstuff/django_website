ce9858dce279af6d95ea116427d9f5d2bc7ae641
Andrey Morozov <yerdnavozorom@gmail.com>
2022-12-21 22:33:06 +0300

    add catalog and product pages

--- shop/shop_cite/views/catalog_page.py
+++ shop/shop_cite/views/catalog_page.py
@@ -1,9 +1,11 @@
 from .view_utils import BaseTemplate
+from ..models import Product
 
 
 class CatalogPage(BaseTemplate):
 
-    def get(self, request):
+    def get(self, request, pk):
+        products = list(Product.objects.filter(category__id=pk))
         return self.get_render(request,
                                'shop_cite/catalog.html',
-                               context={})
+                               context={'products': products})
--- shop/shop_cite/views/product_page.py
+++ shop/shop_cite/views/product_page.py
@@ -1,9 +1,10 @@
 from .view_utils import BaseTemplate
-
+from ..models import Product
 
 class ProductPage(BaseTemplate):
 
-    def get(self, request):
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
         return self.get_render(request,
                                'shop_cite/product.html',
-                               context={})
+                               context={'product': product})
