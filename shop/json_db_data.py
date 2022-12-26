2e74f212ae83989c855167e9316ba6ea3ac449b9
Andrey Morozov <yerdnavozorom@gmail.com>
2022-12-26 23:03:58 +0300

    add subcategories fixtures

--- shop/json_db_data.py
+++ shop/json_db_data.py
@@ -15,6 +15,15 @@ def create_cat_model_object(id_val, parent_category_id, name, image_src, short_n
     return rez
 
 
+def get_main_category_id(main_categories, category_name):
+    cnt = 0
+    for category in main_categories:
+        cnt += 1
+        if category['url_name'] == category_name:
+            return cnt
+    return cnt
+
+
 def create_main_categories_fixture():
     d = os.path.dirname(__file__)
     with open(d + '\\test_data\\main_categories_data.json', 'r', encoding='utf-8') as fp:
@@ -39,4 +48,55 @@ def create_main_categories_fixture():
         data = json.dumps(model_objects, indent=4, ensure_ascii=False).encode('utf8')
         fp.write(data)
 
+
+def create_other_categories_fixture():
+    d = os.path.dirname(__file__)
+    with open(d + '\\test_data\\main_categories_data.json', 'r', encoding='utf-8') as fp:
+        main_categories = json.load(fp)
+
+
+    start_id = 20
+    ids = {}
+    model_objects = []
+
+    for main_category in main_categories:
+        res = []
+        main_category_name = main_category['url_name']
+        with open(d + '\\test_data\\categories_data_{0}.json'.format(main_category_name), 'r', encoding='utf-8') as fp:
+            other_categories = json.load(fp)
+
+        main_cat_id = get_main_category_id(main_categories, main_category_name)
+        ids[main_category_name] = main_cat_id
+        cnt_id = start_id
+        for category in other_categories:
+            root_cat_id = ids.get(category['root'], -1)
+            if root_cat_id == -1:
+                ids[category['root']] = cnt_id
+                root_cat_id = cnt_id
+                cnt_id += 1
+            cat_id = ids.get(category['value']['url_name'], -1)
+            if cat_id == -1:
+                ids[category['value']['url_name']] = cnt_id
+                cat_id = cnt_id
+                cnt_id += 1
+            category['id'] = root_cat_id
+            category['value']['id'] = cat_id
+            res.append(category)
+
+        for val in res:
+            model_objects.append(create_cat_model_object(id_val=val['value']['id'],
+                                                         parent_category_id=val['id'],
+                                                         name=val['value']['human_name'],
+                                                         image_src='assets\\img\\icons\\departments\\plug.svg',
+                                                         short_name=val['value']['url_name']))
+        start_id = cnt_id + 1
+
+        print(len(model_objects), cnt_id - 20)
+
+    with open(d + '\\shop_cite\\fixtures\\other_categories_data_db.json', 'wb') as fp:
+        data = json.dumps(model_objects, indent=4, ensure_ascii=False).encode('utf8')
+        fp.write(data)
+
+
 create_main_categories_fixture()
+create_other_categories_fixture()
