b9f2ec3a91c7e217ff4237406bf741921179d596
Morozov Andrey <yerdnavozorom@gmail.com>
2022-12-26 16:43:45 +0300

    add relative path for created fixtures

--- shop/json_db_data.py
+++ shop/json_db_data.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import os
 
 
 def create_cat_model_object(id_val, parent_category_id, name, image_src, short_name):
@@ -13,24 +14,29 @@ def create_cat_model_object(id_val, parent_category_id, name, image_src, short_n
                      }
     return rez
 
-with open('D:\\python_projects\\python_django_diploma\\shop\\test_data\\main_categories_data.json', 'r', encoding='utf-8') as fp:
-    data = json.load(fp)
 
-model_objects = []
-cnt = 0
-for val in data:
-    cnt += 1
-    file_content = requests.get(val['img_link'])
-    print(val['url_name'])
-    open('D:\\python_projects\\python_django_diploma\\shop\\shop_cite\\static\\shop_cite\\assets\\img\\icons\\departments\\' + val['url_name'] + '.svg', 'wb').write(file_content.content)
+def create_main_categories_fixture():
+    d = os.path.dirname(__file__)
+    with open(d + '\\test_data\\main_categories_data.json', 'r', encoding='utf-8') as fp:
+        data = json.load(fp)
 
-    model_objects.append(create_cat_model_object(id_val=cnt,
-                                                 parent_category_id=None,
-                                                 name=val['human_name'],
-                                                 image_src='assets\\img\\icons\\departments\\' + val['url_name'] + '.svg',
-                                                 short_name=val['url_name']))
+    model_objects = []
+    cnt = 0
+    for val in data:
+        cnt += 1
+        file_content = requests.get(val['img_link'])
+        print(val['url_name'])
+        open(d + '\\shop_cite\\static\\shop_cite\\assets\\img\\icons\\departments\\' + val['url_name'] + '.svg', 'wb').write(file_content.content)
+
+        model_objects.append(create_cat_model_object(id_val=cnt,
+                                                     parent_category_id=None,
+                                                     name=val['human_name'],
+                                                     image_src='assets\\img\\icons\\departments\\' + val['url_name'] + '.svg',
+                                                     short_name=val['url_name']))
 
 
-with open('main_categories_data_db.json', 'wb') as fp:
-    data = json.dumps(model_objects, indent=4, ensure_ascii=False).encode('utf8')
-    fp.write(data)
+    with open(d + '\\shop_cite\\fixtures\\main_categories_data_db.json', 'wb') as fp:
+        data = json.dumps(model_objects, indent=4, ensure_ascii=False).encode('utf8')
+        fp.write(data)
+
+create_main_categories_fixture()
