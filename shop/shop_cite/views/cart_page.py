ea448fe1b55fea6fdc7b062976ac7d1560c90764
Andrey Morozov <yerdnavozorom@gmail.com>
2023-01-21 21:52:17 +0300

    add remove product from cart

--- shop/shop_cite/views/cart_page.py
+++ shop/shop_cite/views/cart_page.py
@@ -14,3 +14,21 @@ class CartPage(BaseTemplate):
         return self.get_render(request,
                                'shop_cite/cart.html',
                                context={'basket_items': basket_items})
+
+    def post(self, request):
+        basket = request.session.get('basket', None)
+        for key in request.POST.keys():
+            if 'delete_item' in key:
+                product_id = key.split('-')[1]
+                del basket[product_id]
+                print(product_id)
+        request.session['basket'] = basket
+        basket_items = []
+        if basket is not None:
+            for product in basket:
+                data = basket[product]
+                data['id'] = product
+                basket_items.append(data)
+        return self.get_render(request,
+                               'shop_cite/cart.html',
+                               context={'basket_items': basket_items})
