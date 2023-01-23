924c153795117497e1ffb094ba825c9903264baa
Andrey Morozov <yerdnavozorom@gmail.com>
2023-01-23 22:49:40 +0300

    add pagination

--- shop/shop_cite/views/catalog_products_page.py
+++ shop/shop_cite/views/catalog_products_page.py
@@ -3,9 +3,12 @@ from ..models import Product, Category
 from ..forms.filter_form import FilterForm
 from django.db.models import Min, Max
 from django.http import HttpResponseRedirect
+from ..app_settings import PAGE_SIZE, PAGE_SHIFT
+from math import ceil
 
 
 class CatalogProductsPage(BaseTemplate):
+
 
     def get_products_cnt_str(self, products_cnt):
         if products_cnt == 0:
@@ -21,6 +24,15 @@ class CatalogProductsPage(BaseTemplate):
             return rez + 'a'
         return rez + 'ов'
 
+    def get_page_number(self, page_val, min_val, max_val):
+        if page_val < min_val:
+            res_page_number = min_val
+        elif page_val > max_val:
+            res_page_number = max_val
+        else:
+            res_page_number = page_val
+        return res_page_number
+
     def get(self, request, pk):
         form = FilterForm()
         search_val = request.GET.get('search', None)
@@ -31,7 +43,7 @@ class CatalogProductsPage(BaseTemplate):
         # get values from get querry or db############
         min_pr = request.GET.get('min_pr', None)
         max_pr = request.GET.get('max_pr', None)
-        page_number = int(request.GET.get('p', 1))
+        cur_page = int(request.GET.get('p', 1))
 
         if min_pr is None or max_pr is None:
             if search_val is None:
@@ -100,45 +112,75 @@ class CatalogProductsPage(BaseTemplate):
             sort_price = 'price_dec'
             order_val = '-price'
         ##############################################
+
         if querry == '':
             if search_val is None:
-                products = list(Product.objects.filter(category=category,
-                                                       price__gte=from_pr,
-                                                       price__lte=to_pr)
-                                .order_by(order_val))
+                data = Product.objects.filter(category=category,
+                                              price__gte=from_pr,
+                                              price__lte=to_pr)
             else:
-                products = list(Product.objects.filter(name__icontains=search_val,
-                                                       price__gte=from_pr,
-                                                       price__lte=to_pr)
-                                .order_by(order_val))
-
+                data = Product.objects.filter(name__icontains=search_val,
+                                              price__gte=from_pr,
+                                              price__lte=to_pr)
         else:
             if search_val is None:
-                products = list(Product.objects.filter(category=category,
-                                                       price__gte=from_pr,
-                                                       price__lte=to_pr,
-                                                       name__icontains=querry)
-                                .order_by(order_val))
-            else:
-                products = list(Product.objects.filter(name__icontains=search_val)
-                                               .filter(price__gte=from_pr,
-                                                       price__lte=to_pr,
-                                                       name__icontains=querry)
-                                               .order_by(order_val))
+                data = Product.objects.filter(category=category,
+                                              price__gte=from_pr,
+                                              price__lte=to_pr,
+                                              name__icontains=querry)
 
+            else:
+                data = Product.objects.filter(name__icontains=search_val).\
+                                       filter(price__gte=from_pr,
+                                              price__lte=to_pr,
+                                              name__icontains=querry)
+        products_cnt = data.count()
+        max_page = int(ceil(products_cnt/PAGE_SIZE))
+        cur_page = self.get_page_number(cur_page, 1, max_page)
+        prev_page = self.get_page_number(cur_page - 1, 1, max_page)
+        next_page = self.get_page_number(cur_page + 1, 1, max_page)
+        start_ind = (cur_page-1)*PAGE_SIZE
+        end_ind = cur_page*PAGE_SIZE - 1
+        if start_ind < 0:
+            start_ind = 0
+        if end_ind >= products_cnt:
+            end_ind = products_cnt - 1
+        products = list(data.order_by(order_val)[start_ind:end_ind + 1])
         if search_val is not None:
             if len(products) > 0:
-                products_cnt_str = self.get_products_cnt_str(len(products))
+                products_cnt_str = self.get_products_cnt_str(products_cnt)
                 category['title'] = 'Найдено: {}'.format(products_cnt_str)
             else:
                 category['title'] = 'Ничего не найдено'
         else:
             if len(products) > 0:
-                products_cnt_str = self.get_products_cnt_str(len(products))
+                products_cnt_str = self.get_products_cnt_str(products_cnt)
                 category.title = 'найдено: {}'.format(products_cnt_str)
             else:
                 category.title = 'ничего не найдено'
             category.title = category.name + ' - ' + category.title
+
+        start_page = cur_page - (PAGE_SHIFT-1)
+        end_page = cur_page + (PAGE_SHIFT-1)
+        left_point_page = None
+        right_point_page = None
+        if start_page < 1:
+            start_page = 1
+        if end_page > max_page:
+            end_page = max_page
+        pages = [(x, '') for x in range(start_page, end_page + 1)]
+        if start_page - 1 >= 2:
+            left_point_page = start_page - 1
+            pages = [(1, ''), ('...', left_point_page)] + pages
+        elif start_page - 1 == 1:
+            pages = [(1, '')] + pages
+        if max_page - end_page >= 2:
+            right_point_page = end_page + 1
+            pages = pages + [('...', right_point_page), (max_page, '')]
+
+        elif max_page - end_page == 1:
+            pages = pages + [(max_page, '')]
+
         return self.get_render(request,
                                'shop_cite/catalog_products.html',
                                context={'products': products,
@@ -158,30 +200,42 @@ class CatalogProductsPage(BaseTemplate):
                                         'new_class': new_class,
                                         'review_class': review_class,
                                         'is_category': search_val is None,
-                                        'cur_page_number': page_number})
+                                        'max_page': max_page,
+                                        'left_point_page': left_point_page,
+                                        'right_point_page': right_point_page,
+                                        'prev_page': prev_page,
+                                        'next_page': next_page,
+                                        'cur_page': cur_page,
+                                        'pages': pages})
 
     def post(self, request, pk):
-
         srch_page = super().post(request)
         if srch_page is None:
             srch_val = request.GET.get('search', '')
             sort_val = request.GET.get('sort', '')
-
             page_number = int(request.GET.get('p', 1))
+
+            is_page_button = False
             for key in request.POST.keys():
                 if 'page' in key:
-                    page_val = key.split('_')[1]
+                    data = key.split('_')
+                    page_val = data[1]
                     if page_val == 'forward':
-                        page_number += 1
-                        if page_number > 3:
-                            page_number = 3
-                    if page_val == 'back':
-                        page_number -= 1
-                        if page_number < 1:
-                            page_number = 1
+                        page_number = int(data[2])
+                    elif page_val == 'back':
+                        page_number = int(data[2])
+                    else:
+                        page_number = int(page_val)
+                    is_page_button = True
                     break
+            if is_page_button:
+                from_pr = request.GET.get('from_pr', None)
+                to_pr = request.GET.get('to_pr', None)
+                min_pr = request.GET.get('min_pr', None)
+                max_pr = request.GET.get('max_pr', None)
+                querry = request.GET.get('name', None)
 
-            if 'filter' in request.POST:
+            elif 'filter' in request.POST:
                 price = request.POST['price'].split(';')
                 if srch_val == '':
                     prices = Product.objects.filter(category__id=pk).aggregate(Max('price'), Min('price'))
